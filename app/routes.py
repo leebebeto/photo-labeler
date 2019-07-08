8d5fc00517e1b7b2339b3a0cd89770e3a7f08cd9
SAMSUNG <bebetot1265@gmail.com>
2019-07-08 13:16:06 +0900

    call data and trying to drag the photos on each side

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,18 @@ Created on Fri May 24 11:25:14 2019
 @author: Kyungmin Park
 """
 
-from flask import render_template
+from flask import render_template, request, url_for, jsonify
 from app import app
+import os
+import json
+import pickle
+import time
+import datetime
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
-    return render_template('photolabeling.html')
+    images = os.listdir(os.path.join(APP_ROOT,'static/image/IU'))
+    keyword_list = ["ATTRACTIVE", "CONFIDENTIAL","RATIONAL","OUT-GOING", "KIND","ADVENTUROUS","STUBBORN"]
+    return render_template('photolabeling.html', images = images, keyword_list = keyword_list)
