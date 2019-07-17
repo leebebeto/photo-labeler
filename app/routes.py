0272a3f120cc5e60a04fc6e1a1b26d3ce9a33ebe
SAMSUNG <bebetot1265@gmail.com>
2019-07-17 12:40:49 +0900

    design revised

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from flask import render_template, request, url_for, jsonify, redirect, flash
+import flask_login
+from flask_login import LoginManager
+from flask_pymongo import PyMongo
 from app import app
 import pymongo
 import os
@@ -8,42 +11,57 @@ import time
 import datetime
 import csv
 
-
-
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+# mongo = PyMongo(app)
+# app.secret_key = os.urandom(24)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 result = []
 
-@app.route('/add', methods = ['GET','POST'])
-def add():
-    client = pymongo.MongoClient('mongodb://localhost:27017/')
-    db = client.newDatabase
-    collection = db.blog_test
-    results = collection.find()
-    new_documents = [
-      {
-        "_id" : "130",
-        "name" : "asd",
-        "content" : "12345"
-      }, {
-        "_id" : "131",
-        "name" : "asd",
-        "content" : "12346"
-      },{
-        "_id" : "1232",
-        "name" : "asd",
-        "content" : "12347"
-      }
-    ]
-    # collection.insert(new_documents)
-    print(results)
-    client.close()
-    return render_template('add.html', data=results)
+
+# class User:
+#     def __init__(self, user_id,passwd_hash=None, authenticated=False):
+#         self.user_id = user_id
+#         self.passwd_hash = passwd_hash
+#         self.authenticated = authenticated
+
+#     def __repr__(self):
+#         r = {
+#             'user_id': self.user_id,
+#             'passwd_hash': self.passwd_hash,
+#             'authenticated': self.authenticated,
+#         }
+#         return str(r)
+
+#     def can_login(self, passwd_hash):
+#         return self.passwd_hash == passwd_hash
+
+#     def is_active(self):
+#         return True
+
+#     def get_id(self):
+#         return self.user_id
+
+#     def is_authenticated(self):
+#         return self.authenticated
+
+#     def is_anonymous(self):
+#         return False
+
+# USERS = {
+#     "user99": User("user99", passwd_hash='davian99'),
+#     "user98": User("user98", passwd_hash='davian98'),
+#     "user97": User("user97", passwd_hash='davian97'),
+# }
+
+# @app.route('/api/login', methods=['POST'])
+# def login():
 
 @app.route('/logIn', methods = ['GET','POST'])
 def logIn():
     return render_template('login.html')
-
 
 @app.route('/logout')
 def logout():
@@ -55,7 +73,11 @@ def getData():
         json_received = request.form
         data = json_received.to_dict(flat=False)
         data_list = json.loads(data['jsonData'][0])
-        print(data_list)
+        client = pymongo.MongoClient('mongodb://localhost:27017/')
+        db = client.newDatabase
+        collection = db.blog_test
+        collection.insert(data_list)
+        client.close()
         outfile = open('labelData.csv', 'a', newline='')
         csvwriter = csv.writer(outfile)
         result.append(data)
