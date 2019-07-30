a4fa1a3213c0e6234ed11cc9623c792dc0970253
SAMSUNG <bebetot1265@gmail.com>
2019-07-30 19:45:22 +0900

    nothing

--- app/routes.py
+++ app/routes.py
@@ -33,8 +33,8 @@ result = []
 batch_number = 14
 
 
-# client = pymongo.MongoClient('mongodb://localhost:27017/')
-client = pymongo.MongoClient("mongodb+srv://admin:davian@daviandb-9rvqg.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+# client = pymongo.MongoClient("mongodb+srv://admin:davian@daviandb-9rvqg.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.davian
 
 
