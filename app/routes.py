e46d3a8ff5f8058e852b60ca60ab88c649b6e700
Kyungmin Park <kyng321@naver.com>
2019-07-31 20:39:55 +0900

    bug fixed

--- app/routes.py
+++ app/routes.py
@@ -22,8 +22,8 @@ import numpy as np
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# client = pymongo.MongoClient('mongodb://localhost:27017/')
-client = pymongo.MongoClient("mongodb+srv://admin:davian@daviandb-9rvqg.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+# client = pymongo.MongoClient("mongodb+srv://admin:davian@daviandb-9rvqg.gcp.mongodb.net/test?retryWrites=true&w=majority")
 
 db = client.davian
 #-------------------------------Parameter---------------------------------------------------
